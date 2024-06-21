20bafcc58017d29bd6c329c38532a47e23a65b12
Ali <iodk123@outlook.com>
2024-06-21 01:46:18 +0100

    added cv and tech stack. also added contact me section.

--- src/pages/contactme.jsx
+++ src/pages/contactme.jsx
@@ -1,58 +1,30 @@
-import React, { useState } from 'react';
-import { Grid, TextField, Button } from '@mui/material';
+import React from 'react';
 
 const Contact = () => {
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'userName') {
-      setUserName(value);
-    }
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission logic here (e.g., send data to backend)
-    console.log('Form submitted with email:', email, 'and username:', userName);
-    // Optionally clear form inputs
-    setEmail('');
-    setUserName('');
-  };
-
   return (
-    <form className="form" onSubmit={handleFormSubmit}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Email"
-            variant="outlined"
-            value={email}
-            name="email"
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Username"
-            variant="outlined"
-            value={userName}
-            name="userName"
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button type="submit" variant="contained" color="primary">
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
+    <section style={{ padding: '20px', background: '#1976d2', margin: '30px 10px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
+      <h2 style={{ marginBottom: '30px', fontSize: '2rem', color: 'white', textAlign: 'center' }}>How to Get in Touch</h2>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <p style={{ lineHeight: '1.6', fontSize: '1.1rem', color: 'white', marginBottom: '10px' }}>
+          <strong>Email:</strong> 
+          <a href="mailto:alimohamedali20041@outlook.com" style={{ color: 'white', marginLeft: '5px', textDecoration: 'underline' }}>
+            alimohamedali20041@outlook.com
+          </a>
+        </p>
+        <p style={{ lineHeight: '1.6', fontSize: '1.1rem', color: 'white', marginBottom: '10px' }}>
+          <strong>Phone Number:</strong> 
+          <a href="tel:+447534054056" style={{ color: 'white', marginLeft: '5px', textDecoration: 'underline' }}>
+            07534054056
+          </a>
+        </p>
+        <p style={{ lineHeight: '1.6', fontSize: '1.1rem', color: 'white', marginBottom: '10px' }}>
+          <strong>LinkedIn:</strong> 
+          <a href="https://www.linkedin.com/in/ali-m-574056289/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', marginLeft: '5px', textDecoration: 'underline' }}>
+            https://www.linkedin.com/in/ali-m-574056289/
+          </a>
+        </p>
+      </div>
+    </section>
   );
 };
 
--- src/pages/cv.jsx
+++ src/pages/cv.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 const Cv = () => {
   return (
-    <section>
-      <a href="path-to-resume.pdf" download>Download Resume</a>
-      <h2>Tech Stack</h2>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>React</li>
+    <section style={{ padding: '20px', background: '#f5f5f5', margin: '30px 10px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
+      <div style={{ textAlign: 'center', marginBottom: '30px' }}>
+        <a href="/cv/Ali Mohamed Ali - cv (3).pdf" download style={{ textDecoration: 'none', color: '#fff', background: '#1976d2', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+          Download Resume
+        </a>
+      </div>
+      <h2 style={{ textAlign: 'center', color: '#333', marginBottom: '20px' }}>Tech Stack</h2>
+      <ul style={{ listStyle: 'none', padding: '0', display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>HTML</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>CSS</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>JavaScript</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>React</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>Node</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>MongoDb</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>postgres</li>
+        <li style={{ background: '#1976d2', color: '#fff', padding: '10px 20px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>sequelize</li>
       </ul>
     </section>
   );
